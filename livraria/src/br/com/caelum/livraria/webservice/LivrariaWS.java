076a029fe7b029d1a144481eaafaa930b5fece4b
Vinicius Jose Schmidt <vinicius.schmidt@cwi.com.br>
2017-03-23 09:32:52 -0300

    WS funcionando (NOTA: JBOSS standalone-preview.xml)

--- livraria/src/br/com/caelum/livraria/webservice/LivrariaWS.java
+++ livraria/src/br/com/caelum/livraria/webservice/LivrariaWS.java
@@ -1,8 +1,22 @@
 package br.com.caelum.livraria.webservice;
 
-import javax.ejb.Stateless;
+import java.util.List;
 
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+
+import br.com.caelum.livraria.modelo.Livro;
+
+@WebService
 @Stateless
 public class LivrariaWS {
 
+	public List<Livro> getLivrosPeloNome(String nome) {
+
+		System.out.println("LivrariaWS: procurando pelo título: " + nome);
+
+		// aqui usaremos o DAO para executar a pesquisa
+
+		return null;
+	}
 }
